83cb8cbc7660ece3b0351291ae9f712fb96d2a2d
FizzingForWurf <zhtong315@gmail.com>
2018-04-29 16:59:41 +0800

    TA4 q3

--- Tutorial/T4A.py
+++ Tutorial/T4A.py
@@ -1,6 +1,7 @@
-#Tutorial 4A
+#! Tutorial 4A
 
-#Question 1
+#? Question 1
+
 '''
 no_of_games = 3
 no_of_players = 3
@@ -44,19 +45,19 @@ for gameNo in range(no_of_games):
 print("%6s" % "Total", end = "")
 for gameNo in range(no_of_games):
     print("%8s" % totalPoints[gameNo], end = "")
-
 '''
 
-#Question 2
+#? Question 2
+
+'''
 no_of_stocks = int(input("Enter the number of stocks: "))
 
-count = 0
 max = 0
 
 names = [" "] * no_of_stocks
 stocks = [[0 for x in range(9)] for y in range(no_of_stocks)]
 
-while(names[count - 1] != "" and count < no_of_stocks):
+for count in range(no_of_stocks):
     names[count] = input("Enter the name of the stock: ")
 
     for day in range(7):
@@ -81,8 +82,6 @@ while(names[count - 1] != "" and count < no_of_stocks):
             if stocks[count][day] > max:
                 max = stocks[count][day]
                 stocks[count][8] = day
-                
-    count += 1
 
 print("%10s%6s%6s%6s%6s%6s%12s%12s" % ("Name", "Mon", "Tue", "Wed", "Thu", "Fri", "Minimum", "Maximum"))
 for stock in range(no_of_stocks):
@@ -91,5 +90,48 @@ for stock in range(no_of_stocks):
         print("%6d" % stocks[stock][day], end = "")
     print("%12s" % (str(stocks[stock][5])+ " (Day: " + str(stocks[stock][7] + 1) + ")"), end = "")
     print("%12s" % (str(stocks[stock][6])+ " (Day: " + str(stocks[stock][8] + 1) + ")"))
+'''
 
-#Question 3
+#? Question 3:
+
+#! REMEMBER: To integrate while loop: 
+#! Solicit first data b4 loop
+#! In the while loop: increment counter var b4 for soliciting again
+
+MAX_STUDENTS = 40
+
+ave = 0.0
+counter = 0
+terminate = True
+
+scores = [0.0] * MAX_STUDENTS
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+scores[counter] = input("Enter the next score or hit 'Enter' to stop: ")
+ave += float(scores[0])
+
+while terminate == True:
+    counter += 1
+    if counter >= MAX_STUDENTS:
+        terminate = False
+
+    student_score = input("Enter the next score or hit 'Enter' to stop: ")
+    if isfloat(student_score):
+        ave += float(student_score)
+        scores[counter] = float(student_score)
+    else:
+        terminate = False
+
+ave /= counter
+print("Class average: " + str(round(ave, 1)))
+print("Scores above average: ", end = "")
+
+for mark in range(counter):
+    if float(scores[mark]) > ave:
+        print(str(scores[mark]) + " ", end = "")
