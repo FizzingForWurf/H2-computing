0742e2995f9c1e2aca10a29194161b541a9c672a
FizzingForWurf <zhtong315@gmail.com>
2019-01-10 13:03:36 +0800

    linked list

--- Examples/linked_list.py
+++ Examples/linked_list.py
@@ -3,19 +3,12 @@ class Node():
     def __init__(self, data, next):
         self.data = data
         self.next = next
-    
-    def data(self):
-        return self.data
-    
-    def next(self):
-        return self.next
 
 def get_list(head):
     probe = head
     while(probe != None):
         print(probe.data, end=' ')
         probe = probe.next
-    print()
     print()
 
 def length(head):
@@ -43,12 +36,28 @@ def insert(index, new_item, head):
     
     return head
 
+def delete(index, head):
+    probe = head
+    prev = None
+
+    for i in range(index-1):
+        prev = probe
+        probe = probe.next
+
+    if prev == None:
+        head = Node(probe.next.data, probe.next.next)
+    else:
+        prev.next = probe.next
+        
+    return head
+
 def main():
     head = None
-    for i in range(2):
+    for i in range(6):
         head = Node(i, head)
     
-    #head = insert(6, 3132, head)
-    print(get_list(head))
+    get_list(head)
+    head = delete(6, head)
+    get_list(head)
 
 main()
